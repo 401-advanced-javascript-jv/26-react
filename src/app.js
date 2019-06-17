b92cfffadaf811eabe24b5a27b23c5e52cc1027f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-17 16:32:10 -0700

    rename Main to Counter, stuff to counter

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ import React from 'react';
 import Header from './header.js';
 import Footer from './footer.js';
 
-class Main extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stuff: 0,
+      counter: 0,
     };
   }
 
   handleButtonClick = (event) => {
     event.preventDefault();
-    this.setState({ stuff: Math.floor(Math.random() * 20 + 1) });
+    this.setState({ counter: Math.floor(Math.random() * 20 + 1) });
   };
 
   render() {
     return (
       <div>
-        <h4>{this.state.stuff}</h4>
+        <h4>{this.state.counter}</h4>
         <button onClick={this.handleButtonClick}>Click Me</button>
       </div>
     );
@@ -31,7 +31,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Header />
-        <Main />
+        <Counter />
         <Footer />
       </React.Fragment>
     );
