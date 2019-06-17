109a355ad9960573583fa68eb03eed856e441610
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-17 16:19:08 -0700

    move footer and header to separate files

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Vinicio - this is just a different way of creating 'dummy components' with functions
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
