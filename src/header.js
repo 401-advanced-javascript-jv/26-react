39761803130dd9cae79c519018e58b4f7bdbc2b5
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-17 16:24:39 -0700

    convert header to class

--- src/header.js
+++ src/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 // Vinicio - this is just a different way of creating 'dummy components' with functions
-const Header = () => {
-  return (
-    <header>
-      <h1>Header</h1>
-    </header>
-  );
-};
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
 
 export default Header;
